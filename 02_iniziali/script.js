c7cef6b89a5617ec0a772bcb25bf2f435d7bf4a4
Aroa Cotrufo <aroa.c97@gmail.com>
2025-12-09 17:34:59 +0100

    feat

--- 02_iniziali/script.js
+++ 02_iniziali/script.js
@@ -5,9 +5,17 @@ const names = ["Anna", "Luca", "Marco", "Adele", "Giovanni", "Alessandra"];
 
 // Dichiara la funzione qui.
 
+function listaIniziali(myArray){
+
+    for (i=0; i<=myArray.lenght; i++){
+        listaInizialiArray.push(myArray[i].charAt(0));
+    }
+    return listaIniziali;
+}
+
 
 // Invoca la funzione qui e stampa il risultato in console
 
-
+console.log(listaIniziali("names"));
 
 //Risultato atteso: ["A", "L", "M", "A", "G", "A"]
