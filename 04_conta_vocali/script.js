8e1845a1408286a4112a291594ba53617214f5d7
Aroa Cotrufo <aroa.c97@gmail.com>
2025-12-10 10:11:20 +0100

    4 esercizio

--- 04_conta_vocali/script.js
+++ 04_conta_vocali/script.js
@@ -4,10 +4,18 @@ const word = 'javascript';
 
 
 // Dichiara la funzione qui.
-
-
+function contatore(val) {
+    const vowels = "aeiou"
+    let count = 0
+for (let i = 0; 0 < val.lenght; i++){
+    if (vowels.includes(val[i])){
+        count++;
+    }
+}
+return count;
+}
 // Invoca la funzione qui e stampa il risultato in console
 
-
+console.log(contatore (word));
 
 //Risultato atteso se si passa 'javascript': 3 (a, a, i)
